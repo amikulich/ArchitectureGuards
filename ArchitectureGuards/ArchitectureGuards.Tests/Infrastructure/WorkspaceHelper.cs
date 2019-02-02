35288d5b3dbe4a68a092c05babfe0aede9d8abf6
Artem Mikulich <artemmikulich@gmail.com>
2019-02-02 23:35:11 +0300

    Project structure

---
+++ ArchitectureGuards/ArchitectureGuards.Tests/Infrastructure/WorkspaceHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
+using NUnit.Framework;
+
+namespace ArchitectureGuards.Tests.Infrastructure
+{
+    public static class WorkspaceHelper
+    {
+        /// <param name="solutionName">Name of a solution file - with or without .sln extension</param>
+        public static Workspace Load(string solutionName)
+        {
+            var solutionFilePath = GetSolutionFilePath(solutionName);
+
+            var workspace = MSBuildWorkspace.Create();
+
+            Assert.DoesNotThrowAsync(async () => await workspace.OpenSolutionAsync(solutionFilePath), "Workspace wasn't loaded correctly. Make sure Microsoft.CodeAnalysis and Microsoft.Build.Tasks libraries are not conflicting with Nuget.Build.Tasks");
+
+            Assert.That(workspace.CurrentSolution.Projects.All(p => p.HasDocuments), $"The following projects have been loaded incorrectly: {string.Join(Environment.NewLine, workspace.CurrentSolution.Projects.Where(p => !p.HasDocuments).Select(p => p.Name))} {Environment.NewLine} See workspace.Diagnostics for details. ");
+
+            return workspace;
+        }
+
+        private static string GetSolutionFilePath(string solutionName)
+        {
+            string repositoryPathRoot = "";
+
+            return Directory.GetFiles(repositoryPathRoot, $"{TrailingSolutionFileExtension.Replace(solutionName, string.Empty)}.sln", SearchOption.AllDirectories).SingleOrDefault();
+        }
+
+        private static Regex TrailingSolutionFileExtension => new Regex(@"\.sln$", RegexOptions.IgnoreCase);
+
+    }
+}
---
+++ ArchitectureGuards/ArchitectureGuards.Tests/RacingClubTests/RacingApiReferenceTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using ArchitectureGuards.Tests.Infrastructure;
+using Microsoft.CodeAnalysis;
+using NUnit.Framework;
+
+namespace ArchitectureGuards.Tests.RacingClubTests
+{
+    [TestFixture]
+    public class RacingApiReferenceTests
+    {
+        Workspace _workspace;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _workspace = WorkspaceHelper.Load("");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _workspace.Dispose();
+        }
+
+        [Test]
+        public void MethodUnderTest_ConditionBeingTested_ExpectedResult()
+        {
+            var domainModelProjects =
+                _workspace.CurrentSolution.Projects.Where(p => p.Name.EndsWith(".DomainModel")).ToList();
+
+            foreach (var project in domainModelProjects)
+            {
+                foreach (var reference in project.ProjectReferences)
+                {
+                    var referencedProject = _workspace.CurrentSolution.GetProject(reference.ProjectId);
+
+                    Assert.Fail(
+                        $"Architecture violation: {project.Name} is a domain model library but references {referencedProject.Name}. You must remove this reference before proceeding.");
+                }
+            }
+
+        }
+    }
+}
